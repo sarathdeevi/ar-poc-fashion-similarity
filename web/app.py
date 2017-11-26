873367af5bff763df67eea8ca8a798448a014fdd
Ankit Srivastava <33308103+anardana@users.noreply.github.com>
2017-11-26 11:39:21 +0530

    Created mobile/upload endpoint

--- web/app.py
+++ web/app.py
@@ -1,31 +1,34 @@
-import os
+import base64
 import shutil
 
 from flask import Flask, render_template, request
 from werkzeug import secure_filename
-from similaritySearch import find_similar
+
 from service import get_label
 
 app = Flask(__name__)
 
 uploadedImageFolder = "static/uploadedImages/"
 
+
 @app.route('/upload')
 def upload_file():
-   return render_template('upload.html')
+    return render_template('upload.html')
+
 
 @app.route('/testupload')
 def test_upload_file():
-   return render_template('test.html')
+    return render_template('test.html')
 
-@app.route('/uploader', methods = ['GET', 'POST'])
+
+@app.route('/uploader', methods=['GET', 'POST'])
 def save_file():
-   if request.method == 'POST':
-      f = request.files['file']
-      
-      f.save(secure_filename(f.filename))
-      shutil.move(f.filename, uploadedImageFolder )
-      return 'file uploaded successfully'
+    if request.method == 'POST':
+        f = request.files['file']
+
+        f.save(secure_filename(f.filename))
+        shutil.move(f.filename, uploadedImageFolder)
+        return 'file uploaded successfully'
 
 
 @app.route('/templatetest')
@@ -33,16 +36,32 @@ def template_test():
     return render_template('similarImagesResults.html', my_string="img.jpg!", my_list=['img.jpg'])
 
 
-@app.route('/find_similar', methods = ['GET', 'POST'])
+@app.route('/find_similar', methods=['GET', 'POST'])
 def find_similar_images():
-	if request.method == 'POST':
-		f = request.files['file']
-		f.save(secure_filename(f.filename))
-		shutil.move(f.filename, uploadedImageFolder + f.filename)
-		similar_images, label_name = get_label(f.filename)
-		return render_template('similarImagesResults.html', input_label=label_name, similar_list=similar_images, input_file=f.filename)
-    
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+        shutil.move(f.filename, uploadedImageFolder + f.filename)
+        similar_images, label_name = get_label(f.filename)
+        return render_template('similarImagesResults.html', input_label=label_name, similar_list=similar_images,
+                               input_file=f.filename)
 
-		
+
+@app.route('/mobile/upload', methods=['POST'])
+def mobile_upload():
+    file_location = "static/uploadedImages/"
+    request_data = base64.b64decode(request.values['image'])
+    file_name = request.values['file_name']
+
+    with open(file_location + file_name, "w") as f:
+        f.write(request_data)
+
+    f.close()
+
+    similar_images, label_name = get_label(file_name)
+    return render_template('similarImagesResults.html', input_label=label_name, similar_list=similar_images,
+                           input_file=file_name)
+
+
 if __name__ == '__main__':
-   app.run(debug = True)
+    app.run(debug=True, host="0.0.0.0", port=80)
